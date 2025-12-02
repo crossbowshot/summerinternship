8846366e77bca427e7a9de78980af5ce335c6c48
crossbowshot <crossbowshot21@gmail.com>
2025-12-02 14:03:13 +0300

    Learned arithmatic with numpy

--- example1/example numpy.py
+++ example1/example numpy.py
@@ -1,1 +1,20 @@
-testing 123
+import numpy as np
+
+# Scalar Arithmatic 
+
+array = np.array([1,2,3])
+
+print(array + 1)
+print (array - 2)
+
+# Vecotrised math functions
+
+array2 = np.array([1.01,2.5,3.99])
+
+print(np.sqrt(array2))
+print(np.round(array2))
+print(np.floor(array2))
+
+# Element Wise arithmetic 
+print(array + array2)
+
