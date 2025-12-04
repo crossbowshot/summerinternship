42700cd91a40e5eae67ce5bfc8c84e178e498fe5
crossbowshot <crossbowshot21@gmail.com>
2025-12-04 13:21:27 +0300

    Created a plot based on pressure levels

--- example1/PandasLearning.py
+++ example1/PandasLearning.py
@@ -1,13 +1,18 @@
 import xarray as xr
 import matplotlib.pyplot as plt
+import cartopy.crs as crs
 
-# dimension = ds.dims
+ds = xr.open_dataset("sresa1b_ncar_ccsm3-example.nc")
 # tempdata = ds.data_vars["tas"].values
 # print(ds)
 # print (dimension)
 # print(tempdata)
 # print(ds.tas)
-ds = xr.open_dataset("sresa1b_ncar_ccsm3-example.nc")
 
-ds['tas'].plot() 
+pressurelvl = 7000
+data_pressurelvl = ds.sel(plev = pressurelvl)
+print(data_pressurelvl)
+data_pressurelvl["ua"].plot()
+print(ds["ua"].dims)
+# ds['tas'].plot() 
 plt.show()
