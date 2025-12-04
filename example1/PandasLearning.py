1c57791c270699b1b1bdefc4cf9d2fca542bc232
crossbowshot <crossbowshot21@gmail.com>
2025-12-04 13:51:09 +0300

    Created a graph of eastward winds at pressure 7000

--- example1/PandasLearning.py
+++ example1/PandasLearning.py
@@ -1,6 +1,6 @@
 import xarray as xr
 import matplotlib.pyplot as plt
-import cartopy.crs as crs
+import cartopy.crs as ccrs
 
 ds = xr.open_dataset("sresa1b_ncar_ccsm3-example.nc")
 # tempdata = ds.data_vars["tas"].values
@@ -11,8 +11,11 @@ ds = xr.open_dataset("sresa1b_ncar_ccsm3-example.nc")
 
 pressurelvl = 7000
 data_pressurelvl = ds.sel(plev = pressurelvl)
+
+plt.figure(figsize=(10,5))
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+
 print(data_pressurelvl)
 data_pressurelvl["ua"].plot()
-print(ds["ua"].dims)
-# ds['tas'].plot() 
 plt.show()
