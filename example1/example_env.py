07d9f05638d19e578f3fa7a3c5c2523391798161
crossbowshot <crossbowshot21@gmail.com>
2025-12-16 15:33:57 +0300

    Added function to create password and add to database and login check against the databas

---
+++ example1/example_env.py
@@ -0,0 +1,46 @@
+import mysql.connector 
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="@Bdull@h1023@$",
+    database = "climate_db"
+
+)
+mycursor = conn.cursor()
+
+Initalinput = input("Hi!, press 1 to sign in or 2 create a new account: ")
+
+if Initalinput == "1":
+    print("111")
+    username = input("Write your username:")
+    password = input("Write your password:")
+    sql = "Select 1 FROM users Where username = %s and password = %s  Limit 1"
+    mycursor.execute(
+        sql,(username,password)
+    )
+    exists = mycursor.fetchone() is not None
+    if exists:
+        print("congrats this exists ")
+        # take to data model 
+        pass
+    else:
+        print("Password or user is incorrect or does not exist")
+        
+
+    # check if it exists then go to climate model 
+elif Initalinput == "2":
+    newusername = input("Create a username:")
+    newpassword = input("Write a password:")
+    sql = "INSERT INTO users (username,password) VALUES (%s,%s)"
+    mycursor.execute(
+        sql,(newusername, newpassword)
+        )
+    conn.commit()
+    print("user created")
+else:
+
+    print("write a proper number ya cunt")
+
+# Create user and password interface that updates database 
+
